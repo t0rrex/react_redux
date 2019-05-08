57beb79328da78b858900bb4a6bd7ab9c2e2db0c
Prometheus <glebus24@mail.ru>
2019-05-08 21:23:22 +0300

    react-select added

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
                 test: /\.js$/,
                 use: ['babel-loader'],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader'],
+                exclude: /node_modules/
             }
         ]
     }
